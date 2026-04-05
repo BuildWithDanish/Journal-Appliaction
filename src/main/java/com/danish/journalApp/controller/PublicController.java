04171086a18c1770a5bc982f1def28c2ae2618e6
Danish Shaikh <danishsince04@gmail.com>
2026-04-05 16:06:39 +0530

    added jwt

--- src/main/java/com/danish/journalApp/controller/PublicController.java
+++ src/main/java/com/danish/journalApp/controller/PublicController.java
@@ -1,10 +1,16 @@
 package com.danish.journalApp.controller;
 
 import com.danish.journalApp.entity.User;
+import com.danish.journalApp.services.UserDetailServiceImpl;
 import com.danish.journalApp.services.UserService;
+import com.danish.journalApp.utils.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,22 +18,44 @@ import java.util.Arrays;
 
 @RestController
 @RequestMapping("/public")
+@Slf4j
 public class PublicController {
     @Autowired
     UserService userService;
 
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailServiceImpl userDetailsService;
 
     @GetMapping
-    public String checkHealth(){
+    public String checkHealth() {
         return "OK";
     }
 
-    @PostMapping("/create-user")
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@RequestBody User user) {
         user.setRoles(Arrays.asList("User"));
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return new ResponseEntity<>(userService.save(user),  HttpStatus.OK);
+        return new ResponseEntity<>(userService.save(user), HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Exception occurred while createAuthenticationToken ", e);
+            return new ResponseEntity<>("Incorrect username or password", HttpStatus.BAD_REQUEST);
+        }
     }
 }
+
